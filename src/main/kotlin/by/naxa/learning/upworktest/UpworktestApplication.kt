3fbf7b8cf4c0b8cd1ccf6448df41a1ec93960f44
naXa! <naxa@nextmail.ru>
2018-09-23 12:31:58 +0300

    Generate project using Spring Initializr (start.spring.io)

---
+++ src/main/kotlin/by/naxa/learning/upworktest/UpworktestApplication.kt
@@ -0,0 +1,11 @@
+package by.naxa.learning.upworktest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UpworktestApplication
+
+fun main(args: Array<String>) {
+    runApplication<UpworktestApplication>(*args)
+}
